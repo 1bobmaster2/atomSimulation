8397bff29ea46a3a21c905a1d3cf818f238b09ae
1bobmaster2 <fkuba556@gmail.com>
2025-04-13 21:45:37 +0200

    debugging and assertions

--- build-all.bat
+++ build-all.bat
@@ -1,17 +1,17 @@
 @ECHO OFF
- REM Build Everything
+REM Build Everything
  
- ECHO "Building everything..."
+ECHO "Building everything..."
  
  
- PUSHD engine
- CALL build.bat
- POPD
- IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+PUSHD engine
+CALL build.bat
+POPD
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
  
- PUSHD testbed
- CALL build.bat
- POPD
- IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+PUSHD testbed
+CALL build.bat
+POPD
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
  
- ECHO "All assemblies built successfully."
+ECHO "All assemblies built successfully."
--- testbed/build.bat
+++ testbed/build.bat
@@ -1,21 +1,21 @@
 REM Build script for testbed
- @ECHO OFF
- SetLocal EnableDelayedExpansion
+@ECHO OFF
+SetLocal EnableDelayedExpansion
  
- REM Get a list of all the .c files.
- SET cFilenames=
- FOR /R %%f in (*.c) do (
-     SET cFilenames=!cFilenames! %%f
- )
+REM Get a list of all the .c files.
+SET cFilenames=
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
  
- REM echo "Files:" %cFilenames%
+REM echo "Files:" %cFilenames%
  
- SET assembly=testbed
- SET compilerFlags=-g 
- REM -Wall -Werror
- SET includeFlags=-Isrc -I../engine/src/
- SET linkerFlags=-L../bin/ -lengine.lib
- SET defines=-D_DEBUG -DKIMPORT
+SET assembly=testbed
+SET compilerFlags=-g 
+REM -Wall -Werror
+SET includeFlags=-Isrc -I../engine/src/
+SET linkerFlags=-L../bin/ -lengine.lib
+SET defines=-D_DEBUG -DKIMPORT
  
- ECHO "Building %assembly%%..."
- clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
+ECHO "Building %assembly%%..."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
